630945d0db8f569051c93da19d809efade097889
MikeR2405 <161502365+MikeR2405@users.noreply.github.com>
2024-05-29 20:30:16 +0300

    Update and rename main.py to homework2.py

---
+++ homework2.py
@@ -0,0 +1,9 @@
+# Объявление переменных
+completed_homeworks = 12
+spent_hours = 1.5
+course_name = 'Python'
+# Вычисление времени на одно задание
+time_per_task = spent_hours / completed_homeworks
+
+# Вывод строки с использованием переменных
+print(f"Курс: {course_name}, всего задач:{completed_homeworks}, затрачено часов: {spent_hours}, среднее время выполнения {time_per_task} часа.")
