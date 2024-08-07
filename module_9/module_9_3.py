7ad30f91e01747fb4be6dd944b628b221d1036bb
MikeR2405 <161502365+MikeR2405@users.noreply.github.com>
2024-08-07 21:43:53 +0300

    Update module_9_3.py

--- module_9/module_9_3.py
+++ module_9/module_9_3.py
@@ -2,7 +2,10 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(i) - len(s) for i, s in zip(first, second) if len(i) != len(s))
-second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
-
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))  # если мы предполагаем, что списки одинаковой длинны
+# second_result = (len(first[i])==len(second[i]) for i in range(min(len(first), len(second)))) - если предполагаем, что списки разные
+# и тогда используется min для определения минимальной из длин этих двух списков. 
+# Это число будет максимальным количеством итераций, которые мы можем выполнить в генераторном выражении, 
+# чтобы не выйти за границы одного из списков.
 print(list(first_result))
 print(list(second_result))
