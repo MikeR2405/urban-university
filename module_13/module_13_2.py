0b948262a9895edf0a8529b377449bee3e9e9c9b
MikeR2405 <161502365+MikeR2405@users.noreply.github.com>
2024-09-29 17:15:27 +0300

    Хендлеры обработки сообщений

---
+++ module_13/module_13_2.py
@@ -0,0 +1,32 @@
+import os
+
+from aiogram import Bot, types, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from dotenv import load_dotenv
+
+load_dotenv()
+api = os.getenv('API_TOKEN')
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(text=['Urban', 'ff'])
+async def urban_message(message):
+    print("Urban message")
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+    print('Start message')
+
+
+@dp.message_handler()
+async def all_message(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    print("Мы получили сообщение!")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
