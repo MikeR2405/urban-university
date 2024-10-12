0d6f7fa1c495ecaaba34dae1c71bc03e9a9532fd
MikeR2405 <161502365+MikeR2405@users.noreply.github.com>
2024-10-12 10:27:12 +0300

    Создание БД
    добавление, выбор и удаление элементов

---
+++ module_14/module_14_1.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+conn = sqlite3.connect('not_telegram.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
+)
+''')
+
+# Insert 10 records into the Users table
+users = [
+    ('User 1', 'example1@gmail.com', 10, 1000),
+    ('User 2', 'example2@gmail.com', 20, 1000),
+    ('User 3', 'example3@gmail.com', 30, 1000),
+    ('User 4', 'example4@gmail.com', 40, 1000),
+    ('User 5', 'example5@gmail.com', 50, 1000),
+    ('User 6', 'example6@gmail.com', 60, 1000),
+    ('User 7', 'example7@gmail.com', 70, 1000),
+    ('User 8', 'example8@gmail.com', 80, 1000),
+    ('User 9', 'example9@gmail.com', 90, 1000),
+    ('User 10', 'example10@gmail.com', 100, 1000)
+]
+
+cursor.executemany('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', users)
+conn.commit()
+
+cursor.execute('UPDATE Users SET balance = 500 WHERE id % 2 = 1')
+conn.commit()
+
+cursor.execute('DELETE FROM Users WHERE id % 3 = 1')
+conn.commit()
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+results = cursor.fetchall()
+
+for user in results:
+    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
+conn.close()
