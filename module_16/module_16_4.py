35ed10111049b5b98886c1d36b449ae19fe1973d
MikeR2405 <161502365+MikeR2405@users.noreply.github.com>
2024-11-10 16:09:46 +0300

    Update module_16_4.py
    внес изменения согласно которым теперь каждый  новый user имеет уникальный id

--- module_16/module_16_4.py
+++ module_16/module_16_4.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 from typing import List
 
 app = FastAPI()
-
 users = []
 
 class User(BaseModel):
@@ -11,19 +10,19 @@ class User(BaseModel):
     username: str
     age: int
 
-
 @app.get("/users", response_model=List[User])
 async def get_all_users() -> List[User]:
     return users
 
-
 @app.post("/user/{username}/{age}", response_model=User)
 async def create_user(username: str, age: int) -> User:
-    user_id = len(users) + 1
+    if users:
+        user_id = max(user.id for user in users) + 1
+    else:
+        user_id = 1
     new_user = User(id=user_id, username=username, age=age)
     users.append(new_user)
     return new_user
-
 
 @app.put("/user/{user_id}/{username}/{age}", response_model=User)
 async def update_user(user_id: int, username: str, age: int) -> User:
@@ -34,7 +33,6 @@ async def update_user(user_id: int, username: str, age: int) -> User:
             return user
     raise HTTPException(status_code=404, detail="User was not found")
 
-
 @app.delete("/user/{user_id}", response_model=User)
 async def delete_user(user_id: int) -> User:
     for user in users:
@@ -42,7 +40,6 @@ async def delete_user(user_id: int) -> User:
             users.remove(user)
             return user
     raise HTTPException(status_code=404, detail="User was not found")
-
 
 @app.delete("/")
 async def delete_all_users() -> str:
