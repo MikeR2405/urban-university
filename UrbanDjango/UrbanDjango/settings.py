a4c5b603fa85795a8ca31b5c06e90d12b1d7537a
MikeR2405 <161502365+MikeR2405@users.noreply.github.com>
2024-11-16 20:46:43 +0300

    Формы отправки данных. HTML и Django формы

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task4',
+    'task5',
 ]
 
 MIDDLEWARE = [
--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -32,10 +32,13 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import path
 from ..task4 import views
+from ..task5 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_view, name='main'),
     path('shop/', views.shop_view, name='shop'),
     path('cart/', views.cart_view, name='cart'),
+    path('', views.sign_up_by_html, name='sign_up_html'),
+    path('django_sign_up/', views.sign_up_by_django, name='sign_up_django'),
 ]
